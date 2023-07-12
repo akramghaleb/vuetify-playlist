e2b82bbd01700b0e1ce043819fd68991da105e96
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-07-12 10:02:43 +0300

    Revert "Update index.vue"
    This reverts commit eb983164ee8b606c98de5e846f6fd4bd0c4fa68a.

--- todo-vuetify/pages/index.vue
+++ todo-vuetify/pages/index.vue
@@ -1,51 +1,5 @@
 <template>
-  <v-app>
-    <v-card
-    color="grey-lighten-4"
-    flat
-    height="200px"
-    rounded="0"
-  >
-    <v-toolbar density="compact">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
-
-  <v-footer class="d-flex flex-column">
-    <div class="bg-teal d-flex w-100 align-center px-4">
-      <strong>Get connected with us on social networks!</strong>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        v-for="icon in icons"
-        :key="icon"
-        class="mx-4"
-        :icon="icon"
-        variant="plain"
-        size="small"
-      ></v-btn>
-    </div>
-
-    <div class="px-4 py-2 bg-black text-center w-100">
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </div>
-  </v-footer>
-  </v-app>
+  <div>
+    Akram Vuetify
+  </div>
 </template>
